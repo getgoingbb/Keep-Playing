c7a13ce37d1692dc8528c32bf6292f7a0699e9f3
BB <113383668+getgoingbb@users.noreply.github.com>
2023-05-11 15:32:08 -0400

    Update config.ts

--- tina/config.ts
+++ tina/config.ts
@@ -6,7 +6,7 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 export default defineConfig({
   branch,
   clientId: "<761ab2af-4a5c-4d10-93fd-1b0af7754096>", // Get this from tina.io
-  token: null: "<79ee4c5e5f00b266e4d405fb65c70cd46c76e006>",
+  token: null: "<9043f2ff45f876fe8d75f27eca12da0d5201a267>",
 
   build: {
     outputFolder: "admin",
